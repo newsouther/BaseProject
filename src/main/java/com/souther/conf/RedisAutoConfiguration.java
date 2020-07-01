78c7e5a16d701897f205d3c8705fe491b07f5fe9
南风 <571691797@qq.com>
2020-07-01 15:42:35 +0800

    update

--- src/main/java/com/souther/conf/RedisAutoConfiguration.java
+++ src/main/java/com/souther/conf/RedisAutoConfiguration.java
@@ -1,20 +1,16 @@
 package com.souther.conf;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisNode;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import redis.clients.jedis.JedisCluster;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @Auther: souther
@@ -44,64 +40,75 @@ public class RedisAutoConfiguration {
   @Value("${spring.redis.jedis.pool.max-wait}")
   private int maxWaitMillis;
 
-  @Bean(name = "myRedisClusterConfiguration")
-  public RedisClusterConfiguration jedisCluster() {
-    RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
-    String[] hosts = host.split(",");
-    Set<RedisNode> nodeList = new HashSet<RedisNode>();
-    for (String hostAndPort : hosts) {
-      String[] hostOrPort = hostAndPort.split(":");
-      nodeList.add(new RedisNode(hostOrPort[0], Integer.parseInt(hostOrPort[1])));
-    }
-    redisClusterConfiguration.setClusterNodes(nodeList);
-    redisClusterConfiguration.setMaxRedirects(maxWaitMillis);
-    return redisClusterConfiguration;
-  }
-
-  @Bean(name = "myJedisPoolConfig")
-  public JedisPoolConfig jedisPoolConfig() {
-    JedisPoolConfig poolConfig = new JedisPoolConfig();
-    poolConfig.setMaxIdle(this.maxIdle);
-    poolConfig.setMinIdle(this.minIdle);
-    poolConfig.setTestOnCreate(true);
-    poolConfig.setTestOnBorrow(true);
-    poolConfig.setTestOnReturn(true);
-    poolConfig.setTestWhileIdle(true);
-    return poolConfig;
-  }
-
-  //  @Bean(name = "MyJedisConnectionFactory")
-//  public JedisConnectionFactory getJedisConnectionFactory(@Qualifier("myRedisClusterConfiguration") RedisClusterConfiguration redisClusterConfiguration,
-//      @Qualifier("myJedisPoolConfig") JedisPoolConfig jedisPoolConfig){
-//    JedisConnectionFactory  jedisConnectionFactory  = new JedisConnectionFactory(redisClusterConfiguration, jedisPoolConfig);
+//  @Bean(name = "myRedisClusterConfiguration")
+//  public RedisClusterConfiguration jedisCluster() {
+//    RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+//    String[] hosts = host.split(",");
+//    Set<RedisNode> nodeList = new HashSet<RedisNode>();
+//    for (String hostAndPort : hosts) {
+//      String[] hostOrPort = hostAndPort.split(":");
+//      nodeList.add(new RedisNode(hostOrPort[0], Integer.parseInt(hostOrPort[1])));
+//    }
+//    redisClusterConfiguration.setClusterNodes(nodeList);
+//    redisClusterConfiguration.setMaxRedirects(maxWaitMillis);
+//    return redisClusterConfiguration;
+//  }
+//
+//  @Bean(name = "myJedisPoolConfig")
+//  public JedisPoolConfig jedisPoolConfig() {
+//    JedisPoolConfig poolConfig = new JedisPoolConfig();
+//    poolConfig.setMaxIdle(this.maxIdle);
+//    poolConfig.setMinIdle(this.minIdle);
+//    poolConfig.setTestOnCreate(true);
+//    poolConfig.setTestOnBorrow(true);
+//    poolConfig.setTestOnReturn(true);
+//    poolConfig.setTestWhileIdle(true);
+//    return poolConfig;
+//  }
+//
+//  @Bean(name = "MyJedisConnectionFactory")
+//  public JedisConnectionFactory getJedisConnectionFactory(
+//      @Qualifier("myRedisClusterConfiguration") RedisClusterConfiguration redisClusterConfiguration,
+//      @Qualifier("myJedisPoolConfig") JedisPoolConfig jedisPoolConfig) {
+//    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(
+//        redisClusterConfiguration, jedisPoolConfig);
 //    // 判断密码是否存在，存在设置值
 //    checkPasswordIfNull(jedisConnectionFactory);
 //    return jedisConnectionFactory;
 //  }
-  @Bean(name = "MyJedisConnectionFactory")
-  public JedisConnectionFactory getJedisConnectionFactory(
-      RedisClusterConfiguration redisClusterConfiguration,
-      @Qualifier("myJedisPoolConfig") JedisPoolConfig jedisPoolConfig) {
-    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(
-        redisClusterConfiguration, jedisPoolConfig);
-    // 判断密码是否存在，存在设置值
-    checkPasswordIfNull(jedisConnectionFactory);
-    return jedisConnectionFactory;
-  }
-
+//
+//
+//  @Bean(name = "myRedisTemplates")
+//  public RedisTemplate redisTemplate(
+//      @Qualifier("MyJedisConnectionFactory") JedisConnectionFactory jedisConnectionFactory) {
+//    StringRedisTemplate myRedisTemplate = new StringRedisTemplate();
+//    myRedisTemplate.setConnectionFactory(jedisConnectionFactory);
+//    return myRedisTemplate;
+//  }
+//
+//  private void checkPasswordIfNull(JedisConnectionFactory jedisConnectionFactory) {
+//    if (!StringUtils.isBlank(password)) {
+//      jedisConnectionFactory.setPassword(password);
+//    }
+//  }
 
   @Bean(name = "myRedisTemplates")
-  public RedisTemplate redisTemplate(
-      @Qualifier("MyJedisConnectionFactory") JedisConnectionFactory jedisConnectionFactory) {
-    StringRedisTemplate myRedisTemplate = new StringRedisTemplate();
-    myRedisTemplate.setConnectionFactory(jedisConnectionFactory);
-    return myRedisTemplate;
-  }
-
-  private void checkPasswordIfNull(JedisConnectionFactory jedisConnectionFactory) {
-    if (!StringUtils.isBlank(password)) {
-      jedisConnectionFactory.setPassword(password);
-    }
+  public RedisTemplate<String, Object> redisTemplate(
+      RedisConnectionFactory redisConnectionFactory) {
+    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+        new Jackson2JsonRedisSerializer<Object>(Object.class);
+    ObjectMapper om = new ObjectMapper();
+    om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+    om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+    jackson2JsonRedisSerializer.setObjectMapper(om);
+    RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+    template.setConnectionFactory(redisConnectionFactory);
+    template.setKeySerializer(jackson2JsonRedisSerializer);
+    template.setValueSerializer(jackson2JsonRedisSerializer);
+    template.setHashKeySerializer(jackson2JsonRedisSerializer);
+    template.setHashValueSerializer(jackson2JsonRedisSerializer);
+    template.afterPropertiesSet();
+    return template;
   }
 
 }
